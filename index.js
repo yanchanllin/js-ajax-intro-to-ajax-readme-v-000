b4718b161a1f1329b086c481307b095d387548dd
YanC Lin <yonalin1@gmail.com>
2018-09-12 19:42:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/Commits');
+  req.send();
+}
+
 function showRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
  var repos = JSON.parse(this.responseText);
  console.log(repos);
   const repoList = `<ul>${repos
-    .map(r => '<li>' + r.name 
+    .map(r => '<li>' + r.name
      '- <a href="#" data-repo="'+
-     r.name + 
+     r.name +
      '" onclick="getCommits(this)">Get Commits</a></li>'
    )
     .join('')}</ul>`;
